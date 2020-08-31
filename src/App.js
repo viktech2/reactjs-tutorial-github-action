3bf49a54bdba28725f8b7c025d82d6b6674604de
Anson <ansfoong@gmail.com>
2020-08-31 04:41:32 -0400

    test commit msg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +16,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <h1>Hello World!!!!!</h1>
+        </div>
       </header>
     </div>
   );
